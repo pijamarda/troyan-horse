b36d73e45c8cadaf026d2691019122e9a2438fc8
pijamarda <zeneke@gmail.com>
2016-12-11 22:50:32 +0100

    new level to create

--- game/scenes/splash_title.gd
+++ game/scenes/splash_title.gd
@@ -3,7 +3,7 @@ extends Node2D
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
-var time = 4
+var time = 5
 
 func _ready():
 	# Called every time the node is added to the scene.
